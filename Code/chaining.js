111762e77963a04486e55b684f1a5a74f17974ad
lenny <lenny.gonzaleznyc@gmail.com>
2021-05-03 23:18:23 -0400

    cleaned up the examples

--- Code/chaining.js
+++ Code/chaining.js
@@ -40,3 +40,80 @@ const halfTheFirstLargeNumber = xs =>
 
 const res = halfTheFirstLargeNumber([1, 4, 50])
 console.log(res)
+
+
+/* ================================================================================= */
+// Straight from Codepen: https://codepen.io/drboolean/pen/poodxOm?editors=0010
+/* ================================================================================= */
+
+const Box = x =>
+({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  toString: () => `Box(${x})`
+})
+
+// Exercise: Box
+// Goal: Refactor each example using Box
+// Keep these tests passing!
+// Bonus points: no curly braces
+
+
+
+
+// Ex1: Using Box, refactor moneyToFloat to be unnested.
+// =========================
+const moneyToFloat_ = str =>
+  parseFloat(str.replace(/\$/, ''))
+
+const moneyToFloat = str =>
+	Box(str)
+	.map(s => s.replace(/\$/, ''))
+	.fold(s => parseFloat(s))
+
+
+QUnit.test("Ex1: moneyToFloat", assert => {
+  assert.equal(String(moneyToFloat('$5.00')), 5)
+})
+
+// Ex2: Using Box, refactor percentToFloat to remove assignment
+// =========================
+const percentToFloat_ = str => {
+  const float = parseFloat(str.replace(/\%/, ''))
+  return float * 0.0100
+}
+
+const percentToFloat = str => [
+	Box(str)
+	.map(s => s.replace(/\%/, ''))
+	.map(s => parseFloat(s))
+	.fold(s => s*0.0100)
+]
+
+QUnit.test("Ex2: percentToFloat", assert => {
+  assert.equal(String(percentToFloat('20%')), 0.2)
+})
+
+
+
+
+
+// Ex3: Using Box, refactor applyDiscount (hint: each variable introduces a new Box)
+// =========================
+const applyDiscount_ = (price, discount) => {
+  const cents = moneyToFloat(price)
+  const savings = percentToFloat(discount)
+  return cents - (cents * savings)
+}
+
+const applyDiscount = (price, discount) =>
+	Box(moneyToFloat(price))
+	.fold(cents =>
+		Box(percentToFloat(discount))
+		.fold(savings => cents - (cents * savings))
+	)
+
+
+QUnit.test("Ex3: Apply discount", assert => {
+  assert.equal(String(applyDiscount('$5.00', '20%')), 4)
+})
