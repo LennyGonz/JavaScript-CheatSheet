997c45874d5752b99c8c48f40e58b720d29f4f19
lenny <lenny.gonzaleznyc@gmail.com>
2021-05-09 15:41:18 -0400

    examples

--- Code/Box.js
+++ Code/Box.js
@@ -80,7 +80,8 @@ const Right = x =>
 ({
   map: f => Right(f(x)),
   inpect: () => `Right(${x})`,
-  fold: f => f(x)
+  fold: (f, g) => g(x),
+  chain: f => f(x)
 })
 
 const rez = Right(3).map(x => x + 1).map(x => x / 2)
@@ -88,11 +89,160 @@ const rez = Right(3).map(x => x + 1).map(x => x / 2)
 const Left = x =>
 ({
   map: f => Left(x),
-  inspect: () => `Left(${x})`
+  chain: f => Left(x),
+  inspect: () => `Left(${x})`,
+  fold: (f, g) => f(x)
 })
 
 // Notice that in Left, it will not run the f on the x -> f(x)
 
 const rezy = Left(3).map(x => x + 1).map(x => x / 2)
 
-// 
+const fromNullable = x =>
+  x != null ? Right(x) : Left(x)
+
+const fs = require('fs')
+
+const getPort_ = () => {
+  try {
+    const str = fs.readFileSync('../JSON/config.json')
+    const config = JSON.parse(str)
+    return config.port
+  } catch (e) {
+    return 3000
+  }
+}
+
+tryCatch = f => {
+  try {
+    return Right(f())
+  } catch (e) {
+    return Left(e)
+  }
+}
+
+const getPort__ = config =>
+  tryCatch(() => fs.readFileSync('../JSON/config.json'))
+    .fold(contents => tryCatch(() => JSON.parse(contents)))
+    .fold(() => 3000, x => x)
+
+// If the config.json does not exist, this will blow up and it isn't ready to catch that error, so we add another tryCatch to be ready for a scenario where config.json doesn't exist
+// However by doing that we nest Box objects so we need to fold twice instead of using map
+// To fix this we add another function called chain
+
+const getPort__ = config =>
+  tryCatch(() => fs.readFileSync('../JSON/config.json'))
+    .chain(contents => tryCatch(() => JSON.parse(contents)))
+    .fold(() => 3000, x => x)
+
+// so isnt chaining and folding the same thing sometimes?
+// well fold - removes a value from its context, taking it out of the box
+// chain expects you to run a function and return another one
+// Even though they have the same definition, they have different functions
+
+const Sum = x =>
+({
+  x,
+  concat: ({ x: y }) => Sum(x + y)
+})
+
+Sum.empty = () => Sum(0)
+
+const Product = x =>
+({
+  x,
+  concat: ({x : y}) => Product(x * y)
+})
+
+Product.empty = () => Product(1)
+
+const Any = x =>
+({
+  x,
+  concat: ({ x: y }) => Any(x || y)
+})
+
+Any.empty = () => Any(false)
+
+const All = x =>
+({
+  x,
+  concat: ({ x: y }) => All(x && y)
+})
+
+All.empty = () => All(true)
+
+const Max = x =>
+({
+  x,
+  concat: ({x : y}) => Max(x > y ? x : y)
+})
+
+Max.empty = () => Max(-Infinity)
+
+const Min = x =>
+({
+  x,
+  concat: ({x : y}) => Max(x < y ? x : y)
+})
+
+Min.empty = () => Min(Infinity)
+
+const Right_ = x =>
+({
+  fold: (f, g) => g(x),
+  map: f => Right(f(x)),
+  concat: o => o.fold(e => Left(e), r => Right(x.concat(r)))
+})
+
+const Left_ = x =>
+({
+  fold: (f, g) => f(x),
+  map: f => Left(x),
+  concat: f => Left(x)
+})
+
+const stats = List.of(
+  { page: 'Home', views: 40 },
+  { page: 'About', views: 10 },
+  { page: 'Blog', views: 4}
+)
+
+stats.foldMap(x =>
+  fromNullable(x.views).map(Sum), Right(Sum(0)))
+
+const First = either =>
+({
+  fold: f => f(either),
+  concat: o => either.isLeft ? o : First(either)
+})
+
+First.empty = () => First(Left())
+
+const find = (xs, f) =>
+  List(xs)
+    .foldMap(x => First(f(x) ? Right(x) : Left()), First.empty())
+    .fold(x => x)
+
+find([3, 4, 5, 6, 7], x => x > 4)
+
+const Fn = f =>
+({
+  fold: f,
+  concat: o => Fn(x => f(x).concat(o.fold(x)))
+})
+
+const hasVowels = x => !!x.match(/[aeiou]/ig)
+const longWord = x => x.length >= 5
+
+const both = Fn(compose(All), hasVowels).concat(Fn(compose(All, longWord)))
+
+['gym', 'bird', 'lilac'].filter(x => both.fold(x).x)
+// lilac
+
+const Pair = (x, y) =>
+({
+  x,
+  y,
+  concat: ({x: x1, y: y1}) => Pair(x.concat(x1), y.concat(y1))
+})
